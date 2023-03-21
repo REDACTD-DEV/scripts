6a17a9e77f3a5a4ed8ec0fea578e628282f1f354
[REDACTED] <22129641+REDACTD-DEV@users.noreply.github.com>
2023-03-21 22:18:19 +1100

    Update pstree-to-mermaidJS.ps1
    added $ProcessID flag, you can now input a PID and get a graphical representation of only that PID, it's parents and children

--- pstree-to-mermaidJS.ps1
+++ pstree-to-mermaidJS.ps1
@@ -1,15 +1,72 @@
 param (
-    [string]$PSTreeFile
+    [string]$PSTreeFile,
+    [string]$ProcessID
 )
 
 # Reads in the contents of $PSTreeFile, skips the first two lines, removes any empty lines, and converts the remaining content into a CSV format with a tab delimiter.
-$CSV = Get-Content $PSTreeFile | Select-Object -skip 2 | Where-Object {$_ -ne ""} | ConvertFrom-Csv -Delimiter "`t" | Select-Object ImageFileName,PID,PPID,CreateTime,Depth
+$CSV = Get-Content $PSTreeFile | Select-Object -skip 2 | Where-Object {$_ -ne ""} | ConvertFrom-Csv -Delimiter "`t" | Select-Object ImageFileName,PID,PPID,CreateTime,Depth,Index
+
+$index=0
+foreach($line in $CSV){
+    # Count the number of asterisks in the PID column to determine the depth level
+    $charcount = ($line.PID.ToCharArray() | Where-Object {$_ -eq '*'} | Measure-Object).Count
+    $line.Depth = $charcount
+
+    # Remove asterisks and whitespace from the PID column
+    $line.PID = $line.PID -replace '\*', ''
+    $line.PID = $line.PID -replace '\s', ''
+    $line.index = $index
+    $index++
+}
+if($ProcessID -ne $null){
+    # Find all parent PIDs for the specified PID
+    $CurrentProcess = $ProcessID
+    $parentPIDs = @()
+    $depth = $CSV | Where-Object pid -eq $CurrentProcess | Select-Object -ExpandProperty depth
+    while ($depth -gt 0){
+        $parent = $CSV | Where-Object pid -eq $CurrentProcess | Select-Object -ExpandProperty PPID
+        $parentPIDs += $parent
+        $CurrentProcess = $parent
+        $depth--
+    }
+
+    # Find all child PIDs for the specified PID
+    $CurrentProcess = $ProcessID
+    $ChildPIDs = @()
+    $depth = $CSV | Where-Object pid -eq $CurrentProcess | Select-Object -ExpandProperty depth
+    $CurrentProcessIndex = ($CSV | Where-Object {$_.pid -eq $CurrentProcess}) | Select-Object -ExpandProperty Index
+    $child = ($CSV | Where-Object {$_.index -eq $CurrentProcessIndex + 1}) | Select-Object
+    while($child.Depth -gt $depth){
+        $ChildPIDs += $child.pid
+        $CurrentProcessIndex++
+        $child = ($CSV | Where-Object {$_.index -eq $CurrentProcessIndex + 1}) | Select-Object
+    }
+
+    # Combine parent, child, and current process IDs into a unique list
+    $PIDList = $parentPIDs + $ChildPIDs + $ProcessID
+    $PIDList = $PIDList | Select-Object -Unique
+
+    # Create an empty array for the custom process tree
+    $CustomPSTree = @()
+
+    # Loop through each process ID and add it to the custom process tree
+    foreach($process in $PIDList){
+        $process = ($CSV | Where-Object {$_.pid -eq $process}) | Select-Object
+        $CustomPSTree += $process
+    }
+
+    # Sort the custom process tree array by the "Index" property
+    $CustomPSTree = $CustomPSTree | Sort-Object -Property Index
+
+    # Update the original CSV variable with the custom process tree
+    $CSV = $CustomPSTree
+}
 
 # Create a new file to hold the MermaidJS code
 new-item -Path . -Name "MermaidJS"
 
 # Add the initial MermaidJS code to the output file
-Write-Output "graph LR" >> MermaidJS
+Write-Output "graph TD" >> MermaidJS
 
 # Define CSS classes for each depth level, with different colors
 Write-Output "`t classDef Depth0 fill:#FFB6C1,color:#000" >> MermaidJS  # Pink
@@ -23,15 +80,6 @@ Write-Output "`t classDef Depth7 fill:#FFA07A,color:#000" >> MermaidJS  # Orange
 
 # Loop through each line of the CSV file
 foreach($line in $CSV) {
-
-    # Count the number of asterisks in the PID column to determine the depth level
-    $charcount = ($line.PID.ToCharArray() | Where-Object {$_ -eq '*'} | Measure-Object).Count
-    $line.Depth = $charcount
-
-    # Remove asterisks and whitespace from the PID column
-    $line.PID = $line.PID -replace '\*', ''
-    $line.PID = $line.PID -replace '\s', ''
-
     # Write comments in the MermaidJS file
     Write-Output "`t %% PPID: $($line.ppid), PID: $($line.pid),  ImageFileName: $($line.ImageFileName), CreateTime: $($line.CreateTime), Depth: $($line.Depth)" >> .\MermaidJS
 
